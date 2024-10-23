3d56cff29f333b047af54ab4273b9d903166bd1f
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-10-24 02:04:28 +0300

    IAuthenticationService oluşturuldu.

---
+++ AuthServer.Core/Dtos/ClientLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthServer.Core.Dtos
+{
+    public class ClientLoginDto
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+    }
+}
