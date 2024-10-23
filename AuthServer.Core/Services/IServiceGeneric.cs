53da6c13d7323ed2caafc6b169b6e79e37d0d6e0
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-10-23 21:16:57 +0300

    IService oluşturuldu.

---
+++ AuthServer.Core/Services/IServiceGeneric.cs
@@ -0,0 +1,22 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+
+        Task<IEnumerable<TDto>> Where(Expression<Func<TEntity, bool>> predicate);
+
+        Task<Response<TDto>> AddAsync(TEntity entity);
+
+        Task<Response<NoDataDto>> Remove(TEntity entity);
+
+        Task<Response<NoDataDto>> Update(TEntity entity);
+    }
+}
