2479b57c39e965ce2eab5b0de2156b456cda3a63
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-10-30 23:01:28 +0300

    UnitOfWork classı oluşturuldu.

---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
