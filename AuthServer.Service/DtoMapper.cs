bbb4ccf08255174acbf2b3fd8bb6778fdbcc7ec5
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-10-31 19:07:00 +0300

    AutoMapper eklendi.

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AutoMapper;
+
+namespace AuthServer.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
