c69dfffa2c33290bbe4fea880a931169a0d9c0a9
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-11-01 20:33:57 +0300

    GetClaim metodu yazıldı.

--- AuthServer.Service/Services/TokenService.cs
+++ AuthServer.Service/Services/TokenService.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using SharedLibrary.Configurations;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace AuthServer.Service.Services
@@ -15,7 +20,7 @@ namespace AuthServer.Service.Services
         private readonly CustomTokenOption _customTokenOption;
         private readonly UserManager<UserApp> _userManager;
 
-        public TokenService(UserManager<UserApp> userManager,IOptions<CustomTokenOption> options)
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
         {
             _userManager = userManager;
             _customTokenOption = options.Value;
@@ -23,12 +28,27 @@ namespace AuthServer.Service.Services
 
         private string CreateRefreshToken()
         {
-            var numberByte=new Byte[32];
-            using var rnd=RandomNumberGenerator.Create();
-            
+            var numberByte = new Byte[32];
+            using var rnd = RandomNumberGenerator.Create();
+
             rnd.GetBytes(numberByte);
 
             return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<String> audiences)
+        {
+            var userList = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email,userApp.Email),
+                new Claim(ClaimTypes.Name,userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            userList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
         }
 
         public TokenDto CreateToken(UserApp userApp)
