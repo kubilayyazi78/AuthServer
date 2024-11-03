fa97b260e1a20b81e0cdd102110ebd389d750669
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-11-03 11:42:09 +0300

    AuthenticationService class oluşturuldu.

---
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,54 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _clients;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _servisGeneric;
+
+        public AuthenticationService(IOptions<List<Client>> options, ITokenService tokenService, UserManager<UserApp> userManager, IUnitOfWork unitOfWork, IGenericRepository<UserRefreshToken> genericRepository)
+        {
+            _clients = options.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _servisGeneric = genericRepository;
+        }
+
+        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
